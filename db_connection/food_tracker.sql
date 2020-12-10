8a63f900b7d1ff8403342901fbfe9592ba2f610e
Karol <krldembski@gmail.com>
2020-12-10 12:58:51 +0100

    update php and db for calendar

--- db_connection/food_tracker.sql
+++ db_connection/food_tracker.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 09, 2020 at 06:56 AM
+-- Generation Time: Dec 10, 2020 at 11:42 AM
 -- Server version: 5.7.31
 -- PHP Version: 7.3.21
 
@@ -24,6 +24,59 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `calendar`
+--
+
+DROP TABLE IF EXISTS `calendar`;
+CREATE TABLE IF NOT EXISTS `calendar` (
+  `date_id` int(63) NOT NULL AUTO_INCREMENT,
+  `day` int(63) DEFAULT NULL,
+  `month` int(63) DEFAULT NULL,
+  `year` int(63) DEFAULT NULL,
+  `dish_id` int(63) DEFAULT NULL,
+  `meal_number` int(2) DEFAULT NULL,
+  PRIMARY KEY (`date_id`),
+  UNIQUE KEY `unique_date_index` (`day`,`month`,`year`,`meal_number`) USING BTREE,
+  KEY `dish_id_calendar_fk` (`dish_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10798 DEFAULT CHARSET=cp1250 COLLATE=cp1250_polish_ci;
+
+--
+-- Dumping data for table `calendar`
+--
+
+INSERT INTO `calendar` (`date_id`, `day`, `month`, `year`, `dish_id`, `meal_number`) VALUES
+(10508, 15, 11, 2020, 2, 0),
+(10686, 16, 11, 2020, 5, 1),
+(10687, 15, 11, 2020, 4, 1),
+(10688, 15, 11, 2020, 2, 2),
+(10689, 16, 11, 2020, 2, 2),
+(10690, 14, 11, 2020, 4, 2),
+(10691, 14, 11, 2020, 2, 1),
+(10794, 8, 11, 2020, 5, 2),
+(10795, 8, 11, 2020, 2, 1),
+(10796, 9, 11, 2020, 4, 2),
+(10797, 9, 11, 2020, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `date_dish_view`
+-- (See below for the actual view)
+--
+DROP VIEW IF EXISTS `date_dish_view`;
+CREATE TABLE IF NOT EXISTS `date_dish_view` (
+`day` int(63)
+,`month` int(63)
+,`year` int(63)
+,`meal_number` int(2)
+,`dish_id` int(63)
+,`dish_name` varchar(255)
+,`image` varchar(255)
+);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `dishes`
 --
 
@@ -34,22 +87,26 @@ CREATE TABLE IF NOT EXISTS `dishes` (
   `preparation_time` int(63) DEFAULT NULL,
   `rating` int(2) DEFAULT NULL,
   `image` varchar(255) COLLATE cp1250_polish_ci DEFAULT NULL,
+  `portions` int(63) DEFAULT NULL,
   PRIMARY KEY (`dish_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=cp1250 COLLATE=cp1250_polish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=126 DEFAULT CHARSET=cp1250 COLLATE=cp1250_polish_ci;
 
 --
 -- Dumping data for table `dishes`
 --
 
-INSERT INTO `dishes` (`dish_id`, `dish_name`, `preparation_time`, `rating`, `image`) VALUES
-(2, 'Makaron z kurczakiem i warzywami', 100, 5, 'https://s3.przepisy.pl/przepisy3ii/img/variants/800x0/zupa-krem-z-marchewki-z-migdalami.jpg'),
-(4, 'Makaron z kurczakiem i szpinakiem', 30, 4, 'https://www.przyslijprzepis.pl/media/cache/default_view/uploads/media/recipe/0006/40/f21b0c6846ee2fe2af5147d71df1687ab25d5c79.jpeg'),
-(5, 'Zupa krem z pomidorów', 123, 1, 'https://s3.przepisy.pl/przepisy3ii/img/variants/800x0/zupa-krem-z-marchewki-z-migdalami.jpg'),
-(7, 'Zupa krem z marchewek', 456, 5, 'https://s3.przepisy.pl/przepisy3ii/img/variants/800x0/chrupiace-kurczaki.jpg'),
-(8, 'Ramen', 234, 3, 'https://s3.przepisy.pl/przepisy3ii/img/variants/800x0/chrupiace-kurczaki.jpg'),
-(9, 'Racuchy z jabłkami', 345, 2, 'https://s3.przepisy.pl/przepisy3ii/img/variants/800x0/chrupiace-kurczaki.jpg'),
-(10, 'Makaron z kurczakiem i warzywami', 312, 2, 'https://s3.przepisy.pl/przepisy3ii/img/variants/800x0/zupa-krem-z-marchewki-z-migdalami.jpg'),
-(11, 'Makaron z kurczakiem i warzywami', 354, 3, '');
+INSERT INTO `dishes` (`dish_id`, `dish_name`, `preparation_time`, `rating`, `image`, `portions`) VALUES
+(2, 'Makaron z kurczakiem i warzywami1', 100, 2, 'http://localhost/food_tracker/Bez%C2%A0tytu%C5%82u.jpg', 2),
+(4, 'Makaron z kurczakiem i szpinakiem2', 123, 5, 'https://s3.przepisy.pl/przepisy3ii/img/variants/800x0/zupa-krem-z-marchewki-z-migdalami.jpg', 4),
+(5, 'Zupa krem z pomidorów', 323, 1, 'https://s3.przepisy.pl/przepisy3ii/img/variants/800x0/zupa-krem-z-marchewki-z-migdalami.jpg', 4),
+(7, 'Zupa krem z marchewek', 40, 5, 'https://s3.przepisy.pl/przepisy3ii/img/variants/800x0/chrupiace-kurczaki.jpg', 2),
+(9, 'Racuchy', 345, 2, 'https://s3.przepisy.pl/przepisy3ii/img/variants/800x0/chrupiace-kurczaki.jpg', 2),
+(10, 'Makaron z kurczakiem i warzywa', 12, 1, '', 4),
+(11, 'Makaron z kurczakiem i warzywami', 354, 3, 'https://s3.przepisy.pl/przepisy3ii/img/variants/800x0/zupa-krem-z-marchewki-z-migdalami.jpg', 2),
+(12, 'Makaron z kurczakiem i warzywami', 354, 3, 'https://s3.przepisy.pl/przepisy3ii/img/variants/800x0/zupa-krem-z-marchewki-z-migdalami.jpg', 2),
+(13, 'Makaron z kurczakiem i warzywami', 354, 3, 'https://s3.przepisy.pl/przepisy3ii/img/variants/800x0/zupa-krem-z-marchewki-z-migdalami.jpg', 2),
+(14, 'Makaron z kurczakiem i warzywami', 354, 5, 'https://s3.przepisy.pl/przepisy3ii/img/variants/800x0/zupa-krem-z-marchewki-z-migdalami.jpg', 22),
+(125, 'wwwww wwwww wwwwww wwwwww wwwww wwwwwwwww', 1, 1, '1', 1);
 
 -- --------------------------------------------------------
 
@@ -65,7 +122,7 @@ CREATE TABLE IF NOT EXISTS `ingredients` (
   `kcal_per_unit` float(63,2) NOT NULL,
   PRIMARY KEY (`ingredient_id`),
   KEY `unit_id_fk` (`unit_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=cp1250 COLLATE=cp1250_polish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=cp1250 COLLATE=cp1250_polish_ci;
 
 --
 -- Dumping data for table `ingredients`
@@ -76,8 +133,23 @@ INSERT INTO `ingredients` (`ingredient_id`, `ingredient_name`, `unit_id`, `kcal_
 (2, 'czosnek', 3, 8.00),
 (3, 'ser żółty', 1, 3.80),
 (4, 'makaron', 1, 3.50),
-(5, 'szpinak', 1, 0.24),
+(5, 'szpinak', 1, 1.00),
 (6, 'jogurt naturalny - kremowy', 1, 0.60);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `ingredients_view`
+-- (See below for the actual view)
+--
+DROP VIEW IF EXISTS `ingredients_view`;
+CREATE TABLE IF NOT EXISTS `ingredients_view` (
+`ingredient_id` int(63)
+,`ingredient_name` varchar(63)
+,`kcal_per_unit` float(63,2)
+,`unit_name` varchar(255)
+,`unit_id` int(63)
+);
 
 -- --------------------------------------------------------
 
@@ -90,21 +162,23 @@ CREATE TABLE IF NOT EXISTS `recipes_ingredients` (
   `dish_id` int(63) NOT NULL,
   `ingredient_id` int(63) NOT NULL,
   `amount` int(63) DEFAULT NULL,
-  KEY `dish_id_fk2` (`dish_id`),
-  KEY `ingredients_id_fk` (`ingredient_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=cp1250 COLLATE=cp1250_polish_ci;
+  `recipe_element_id` int(63) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`recipe_element_id`),
+  KEY `ingredients_id_fk` (`ingredient_id`),
+  KEY `dish_id_ingredients_fk` (`dish_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=cp1250 COLLATE=cp1250_polish_ci;
 
 --
 -- Dumping data for table `recipes_ingredients`
 --
 
-INSERT INTO `recipes_ingredients` (`dish_id`, `ingredient_id`, `amount`) VALUES
-(4, 1, 600),
-(4, 4, 500),
-(4, 3, 40),
-(4, 5, 500),
-(4, 6, 400),
-(4, 2, 3);
+INSERT INTO `recipes_ingredients` (`dish_id`, `ingredient_id`, `amount`, `recipe_element_id`) VALUES
+(4, 3, 1, 1),
+(4, 1, 1, 20),
+(4, 3, 1, 33),
+(2, 4, 1, 77),
+(2, 5, 1, 95),
+(2, 6, 1, 96);
 
 -- --------------------------------------------------------
 
@@ -114,9 +188,11 @@ INSERT INTO `recipes_ingredients` (`dish_id`, `ingredient_id`, `amount`) VALUES
 --
 DROP VIEW IF EXISTS `recipes_ingredients_view`;
 CREATE TABLE IF NOT EXISTS `recipes_ingredients_view` (
-`dish_id` int(63)
+`recipe_element_id` int(63)
+,`dish_id` int(63)
 ,`amount` int(63)
 ,`ingredient_name` varchar(63)
+,`ingredient_id` int(63)
 ,`kcal_per_unit` float(63,2)
 ,`unit_name` varchar(255)
 ,`shortcut` varchar(63)
@@ -135,19 +211,22 @@ CREATE TABLE IF NOT EXISTS `recipes_steps` (
   `instructions` varchar(255) COLLATE cp1250_polish_ci DEFAULT NULL,
   `dish_id` int(63) NOT NULL,
   PRIMARY KEY (`step_id`),
-  KEY `dish_id_fk` (`dish_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=cp1250 COLLATE=cp1250_polish_ci;
+  KEY `dish_id_steps_fk` (`dish_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=83 DEFAULT CHARSET=cp1250 COLLATE=cp1250_polish_ci;
 
 --
 -- Dumping data for table `recipes_steps`
 --
 
 INSERT INTO `recipes_steps` (`step_id`, `step_number`, `instructions`, `dish_id`) VALUES
-(1, 1, 'Piersi kurczaka umyj, osusz, pokrój w cienkie paseczki, obsyp solą i pieprzem.', 4),
 (2, 2, 'Na patelnię wlej odrobinę oliwy z oliwek, wrzuć mięso. Kawałki kurczaka grilluj przez kilka minut, aż nabiorą ładnego koloru. Ściągnij kurczaka z patelni.', 4),
-(3, 3, 'Na patelnię wlej łyżkę oliwy z oliwek, dodaj posiekany czosnek i szpinak. Całość smaż przez 2 minuty, stale mieszając.', 4),
-(4, 4, 'Do szpinaku dodaj jogurt i mięso z kurczaka. Sos gotuj przez kilka minut. Dopraw do smaku.', 4),
-(5, 5, 'Makaron ugotuj al dente i wymieszaj z sosem. Podawaj posypane startym serem żółtym.', 4);
+(3, 1, 'Na patelnię wlej łyżkę oliwy z oliwek, dodaj posiekany czosnek i szpinak. Całość smaż przez 2 minuty, stale mieszając.', 4),
+(51, 1, '123', 2),
+(52, 2, '123', 2),
+(53, 3, '123', 2),
+(66, 3, '123123123', 4),
+(70, 4, '123', 2),
+(82, 4, '123', 4);
 
 -- --------------------------------------------------------
 
@@ -161,7 +240,7 @@ CREATE TABLE IF NOT EXISTS `unit` (
   `unit_name` varchar(255) COLLATE cp1250_polish_ci NOT NULL,
   `shortcut` varchar(63) COLLATE cp1250_polish_ci NOT NULL,
   PRIMARY KEY (`unit_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=cp1250 COLLATE=cp1250_polish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=cp1250 COLLATE=cp1250_polish_ci;
 
 --
 -- Dumping data for table `unit`
@@ -175,35 +254,61 @@ INSERT INTO `unit` (`unit_id`, `unit_name`, `shortcut`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Structure for view `date_dish_view`
+--
+DROP TABLE IF EXISTS `date_dish_view`;
+
+DROP VIEW IF EXISTS `date_dish_view`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `date_dish_view`  AS  select `calendar`.`day` AS `day`,`calendar`.`month` AS `month`,`calendar`.`year` AS `year`,`calendar`.`meal_number` AS `meal_number`,`dishes`.`dish_id` AS `dish_id`,`dishes`.`dish_name` AS `dish_name`,`dishes`.`image` AS `image` from (`calendar` left join `dishes` on((`dishes`.`dish_id` = `calendar`.`dish_id`))) ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `ingredients_view`
+--
+DROP TABLE IF EXISTS `ingredients_view`;
+
+DROP VIEW IF EXISTS `ingredients_view`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `ingredients_view`  AS  select `ingredients`.`ingredient_id` AS `ingredient_id`,`ingredients`.`ingredient_name` AS `ingredient_name`,`ingredients`.`kcal_per_unit` AS `kcal_per_unit`,`unit`.`unit_name` AS `unit_name`,`unit`.`unit_id` AS `unit_id` from (`ingredients` left join `unit` on((`ingredients`.`unit_id` = `unit`.`unit_id`))) ;
+
+-- --------------------------------------------------------
+
+--
 -- Structure for view `recipes_ingredients_view`
 --
 DROP TABLE IF EXISTS `recipes_ingredients_view`;
 
 DROP VIEW IF EXISTS `recipes_ingredients_view`;
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `recipes_ingredients_view`  AS  select `recipes_ingredients`.`dish_id` AS `dish_id`,`recipes_ingredients`.`amount` AS `amount`,`ingredients`.`ingredient_name` AS `ingredient_name`,`ingredients`.`kcal_per_unit` AS `kcal_per_unit`,`unit`.`unit_name` AS `unit_name`,`unit`.`shortcut` AS `shortcut` from ((`recipes_ingredients` left join `ingredients` on((`recipes_ingredients`.`ingredient_id` = `ingredients`.`ingredient_id`))) left join `unit` on((`ingredients`.`unit_id` = `unit`.`unit_id`))) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `recipes_ingredients_view`  AS  select `recipes_ingredients`.`recipe_element_id` AS `recipe_element_id`,`recipes_ingredients`.`dish_id` AS `dish_id`,`recipes_ingredients`.`amount` AS `amount`,`ingredients`.`ingredient_name` AS `ingredient_name`,`ingredients`.`ingredient_id` AS `ingredient_id`,`ingredients`.`kcal_per_unit` AS `kcal_per_unit`,`unit`.`unit_name` AS `unit_name`,`unit`.`shortcut` AS `shortcut` from ((`recipes_ingredients` left join `ingredients` on((`recipes_ingredients`.`ingredient_id` = `ingredients`.`ingredient_id`))) left join `unit` on((`ingredients`.`unit_id` = `unit`.`unit_id`))) ;
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `calendar`
+--
+ALTER TABLE `calendar`
+  ADD CONSTRAINT `dish_id_calendar_fk` FOREIGN KEY (`dish_id`) REFERENCES `dishes` (`dish_id`) ON DELETE CASCADE;
+
+--
 -- Constraints for table `ingredients`
 --
 ALTER TABLE `ingredients`
-  ADD CONSTRAINT `unit_id_fk` FOREIGN KEY (`unit_id`) REFERENCES `unit` (`unit_id`);
+  ADD CONSTRAINT `unit_id_fk` FOREIGN KEY (`unit_id`) REFERENCES `unit` (`unit_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `recipes_ingredients`
 --
 ALTER TABLE `recipes_ingredients`
-  ADD CONSTRAINT `dish_id_fk2` FOREIGN KEY (`dish_id`) REFERENCES `dishes` (`dish_id`),
-  ADD CONSTRAINT `ingredients_id_fk` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`ingredient_id`);
+  ADD CONSTRAINT `dish_id_ingredients_fk` FOREIGN KEY (`dish_id`) REFERENCES `dishes` (`dish_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `ingredients_id_fk` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`ingredient_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `recipes_steps`
 --
 ALTER TABLE `recipes_steps`
-  ADD CONSTRAINT `dish_id_fk` FOREIGN KEY (`dish_id`) REFERENCES `dishes` (`dish_id`);
+  ADD CONSTRAINT `dish_id_steps_fk` FOREIGN KEY (`dish_id`) REFERENCES `dishes` (`dish_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- db_connection/process.php
+++ db_connection/process.php
@@ -20,15 +20,17 @@ if($action == 'get_all_dishes'){
 }
 
 if($action == 'add_dish'){
-  $name = $_POST['name'];
-  $prepTime = $_POST['prepTime'];
-  $rating = $_POST['rating'];
+  $name = $_POST['dish_name'];
   $image = $_POST['image'];
+  $prepTime = $_POST['preparation_time'];
+  $rating = $_POST['rating'];
+  $portions = $_POST['portions'];
 
-  $sql = $conn->query("INSERT INTO dishes (dish_name, preparation_time, rating, image) VALUES('$name','$prepTime','$rating','$image')");
-
+  $sql = $conn->query("INSERT INTO dishes (dish_name, preparation_time, rating, image, portions) VALUES('$name','$prepTime','$rating','$image','$portions')");
+  
   if($sql){
-    $result['message'] = "Dish added successfully!";
+    $added_id = $conn->insert_id;
+    $result['added_id'] = $added_id;
   }
   else{
     $result['error'] = true;
@@ -42,8 +44,9 @@ if($action == 'update_dish'){
   $image = $_POST['image'];
   $prepTime = $_POST['preparation_time'];
   $rating = $_POST['rating'];
+  $portions = $_POST['portions'];
 
-  $sql = $conn->query("UPDATE dishes SET dish_name='$name',preparation_time='$prepTime',rating='$rating',image='$image' WHERE dish_id='$id'");
+  $sql = $conn->query("UPDATE dishes SET dish_name='$name',preparation_time='$prepTime',rating='$rating',image='$image',portions='$portions' WHERE dish_id='$id'");
 
   if($sql){
     $result['message'] = "Dish updated successfully!";
@@ -55,7 +58,7 @@ if($action == 'update_dish'){
 }
 
 if($action == 'delete_dish'){
-  $id = $_POST['id'];
+  $id = $_POST['dish_id'];
 
   $sql = $conn->query("DELETE FROM dishes WHERE dish_id='$id'");
 
@@ -81,7 +84,7 @@ if($action == 'dish_details'){
   while($row = $sql->fetch_assoc()){
     array_push($dish, $row);
   }
-  $sql = $conn->query("SELECT amount, ingredient_name, kcal_per_unit, unit_name, shortcut FROM recipes_ingredients_view WHERE dish_id='$id'");
+  $sql = $conn->query("SELECT * FROM recipes_ingredients_view WHERE dish_id='$id' ORDER BY recipe_element_id");
   $dish_ingredients = array();
   while($row = $sql->fetch_assoc()){
     array_push($dish_ingredients, $row);
@@ -89,6 +92,269 @@ if($action == 'dish_details'){
   $result['dish'] = $dish;
   $result['dish_steps'] = $dish_steps;
   $result['dish_ingredients'] = $dish_ingredients;
+}
+
+if($action == 'get_dish_ingredients'){
+  $id = $_POST['dish_id'];
+
+  $sql = $conn->query("SELECT * FROM recipes_ingredients_view WHERE dish_id='$id' ORDER BY recipe_element_id");
+  $dish_ingredients = array();
+  while($row = $sql->fetch_assoc()){
+    array_push($dish_ingredients, $row);
+  }
+  $result['dish_ingredients'] = $dish_ingredients;
+}
+
+if($action == 'update_step'){
+  $id = $_POST['step_id'];
+  $number = $_POST['step_number'];
+  $instructions = $_POST['instructions'];
+
+  $sql = $conn->query("UPDATE recipes_steps SET step_number='$number',instructions='$instructions' WHERE step_id='$id'");
+
+  if($sql){
+    $result['message'] = "Step updated successfully!";
+  }
+  else{
+    $result['error'] = true;
+    $result['message'] = "Failed to update step!";
+  }
+}
+
+if($action == 'add_step'){
+  $number = $_POST['step_number'];
+  $instructions = $_POST['instructions'];
+  $dishid = $_POST['dish_id'];
+
+  $sql = $conn->query("INSERT INTO recipes_steps (step_number, instructions, dish_id) VALUES('$number','$instructions','$dishid')");
+
+  if($sql){
+    $result['message'] = "Step added successfully!";
+  }
+  else{
+    $result['error'] = true;
+    $result['message'] = "Failed to add step!";
+  }
+}
+
+if($action == 'delete_step'){
+  $id = $_POST['step_id'];
+
+  $sql = $conn->query("DELETE FROM recipes_steps WHERE step_id='$id'");
+
+  if($sql){
+    $result['message'] = "Step deleted successfully!";
+  }
+  else{
+    $result['error'] = true;
+    $result['message'] = "Failed to delete step!";
+  }
+}
+
+if($action == 'get_ingredients'){
+  $sql = $conn->query("SELECT * FROM ingredients_view");
+  $ingredients = array();
+  while($row = $sql->fetch_assoc()){
+    array_push($ingredients, $row);
+  }
+  $result['ingredients'] = $ingredients;
+}
+
+if($action == 'get_units'){
+  $sql = $conn->query("SELECT * FROM unit");
+  $units = array();
+  while($row = $sql->fetch_assoc()){
+    array_push($units, $row);
+  }
+  $result['units'] = $units;
+}
+
+if($action == 'update_ingredient'){
+  $recipe_element_id = $_POST['recipe_element_id'];
+  $ingredient_id = $_POST['ingredient_id'];
+  $amount = $_POST['amount'];
+
+  $sql = $conn->query("UPDATE recipes_ingredients SET ingredient_id='$ingredient_id', amount='$amount' WHERE recipe_element_id='$recipe_element_id'");
+
+  if($sql){
+    $result['message'] = "Ingredient updated successfully!";
+  }
+  else{
+    $result['error'] = true;
+    $result['message'] = "Failed to update ingredient!";
+  }
+}
+
+if($action == 'add_ingredient'){
+  $dish_id = $_POST['dish_id'];
+  $ingredient_id = $_POST['ingredient_id'];
+  $amount = $_POST['amount'];
+
+  $sql = $conn->query("INSERT INTO recipes_ingredients (ingredient_id, amount, dish_id) VALUES('$ingredient_id','$amount','$dish_id')");
+
+  if($sql){
+    $result['message'] = "Ingredient added successfully!";
+  }
+  else{
+    $result['error'] = true;
+    $result['message'] = "Failed to add ingredient!";
+  }
+}
+
+if($action == 'delete_ingredient'){
+  $recipe_element_id = $_POST['recipe_element_id'];
+
+  $sql = $conn->query("DELETE FROM recipes_ingredients WHERE recipe_element_id='$recipe_element_id'");
+
+  if($sql){
+    $result['message'] = $recipe_element_id;
+  }
+  else{
+    $result['error'] = true;
+    $result['message'] = "Failed to delete ingredient!";
+  }
+}
+
+if($action == 'get_day'){
+  $day = $_POST['day'];
+  $month = $_POST['month'];
+  $year = $_POST['year'];
+  $index = $_POST['index'];
+
+  $sql = $conn->query("SELECT dish_id, dish_name, image FROM date_dish_view WHERE day = '$day' AND month = '$month' AND year = '$year' AND meal_number = '$index'");
+  $day = array();
+  while($row = $sql->fetch_assoc()){
+    array_push($day, $row);
+  }
+  $result['day'] = $day;
+}
+
+
+if($action == 'replace_day'){
+  $day = $_POST['day'];
+  $month = $_POST['month'];
+  $year = $_POST['year'];
+  $index = $_POST['index'];
+  $dish_id = $_POST['dish_id'];
+
+  $sql = $conn->query("REPLACE INTO calendar (day, month, year, meal_number, dish_id) VALUES ('$day','$month','$year','$index','$dish_id')");
+
+  if($sql){
+    $result['message'] = "Day replaced successfully!";
+  }
+  else{
+    $result['error'] = true;
+    $result['message'] = "Failed to replace day!";
+  }
+}
+
+if($action == 'delete_day'){
+  $day = $_POST['day'];
+  $month = $_POST['month'];
+  $year = $_POST['year'];
+  $index = $_POST['index'];
+
+  $sql = $conn->query("DELETE FROM calendar WHERE day = '$day' AND month = '$month' AND year = '$year' AND meal_number = '$index'");
+
+  if($sql){
+    $result['message'] = "Day deleted successfully!";
+  }
+  else{
+    $result['error'] = true;
+    $result['message'] = "Failed to delete day!";
+  }
+}
+
+if($action == 'update_global_ingredient'){
+  $ingredient_id = $_POST['ingredient_id'];
+  $ingredient_name = $_POST['ingredient_name'];
+  $unit_id = $_POST['unit_id'];
+  $kcal_per_unit = $_POST['kcal_per_unit'];
+
+  $sql = $conn->query("UPDATE ingredients SET ingredient_name='$ingredient_name', unit_id='$unit_id', kcal_per_unit='$kcal_per_unit' WHERE ingredient_id='$ingredient_id'");
+
+  if($sql){
+    $result['message'] = "Ingredient updated successfully!";
+  }
+  else{
+    $result['error'] = true;
+    $result['message'] = "Failed to update ingredient!";
+  }
+}
+
+if($action == 'add_global_ingredient'){
+  $ingredient_name = $_POST['ingredient_name'];
+  $unit_id = $_POST['unit_id'];
+  $kcal_per_unit = $_POST['kcal_per_unit'];
+
+  $sql = $conn->query("INSERT INTO ingredients (ingredient_name, unit_id, kcal_per_unit) VALUES ('$ingredient_name', '$unit_id', '$kcal_per_unit')");
+
+  if($sql){
+    $result['message'] = "Ingredient added successfully!";
+  }
+  else{
+    $result['error'] = true;
+    $result['message'] = "Failed to add ingredient!";
+  }
+}
+
+if($action == 'delete_global_ingredient'){
+  $ingredient_id = $_POST['ingredient_id'];
+
+  $sql = $conn->query("DELETE FROM ingredients WHERE ingredient_id='$ingredient_id'");
+
+  if($sql){
+    $result['message'] = $ingredient_id;
+  }
+  else{
+    $result['error'] = true;
+    $result['message'] = "Failed to delete ingredient!";
+  }
+}
+
+if($action == 'update_unit'){
+  $unit_id = $_POST['unit_id'];
+  $unit_name = $_POST['unit_name'];
+  $shortcut = $_POST['shortcut'];
+
+  $sql = $conn->query("UPDATE unit SET unit_name='$unit_name', shortcut='$shortcut' WHERE unit_id='$unit_id'");
+
+  if($sql){
+    $result['message'] = "Unit updated successfully!";
+  }
+  else{
+    $result['error'] = true;
+    $result['message'] = "Failed to update unit!";
+  }
+}
+
+if($action == 'add_unit'){
+  $unit_name = $_POST['unit_name'];
+  $shortcut = $_POST['shortcut'];
+
+  $sql = $conn->query("INSERT INTO unit (unit_name, shortcut) VALUES ('$unit_name','$shortcut')");
+
+  if($sql){
+    $result['message'] = "Unit added successfully!";
+  }
+  else{
+    $result['error'] = true;
+    $result['message'] = "Failed to add unit!";
+  }
+}
+
+if($action == 'delete_unit'){
+  $unit_id = $_POST['unit_id'];
+
+  $sql = $conn->query("DELETE FROM unit WHERE unit_id='$unit_id'");
+
+  if($sql){
+    $result['message'] = $unit_id;
+  }
+  else{
+    $result['error'] = true;
+    $result['message'] = "Failed to delete unit!";
+  }
 }
 
 $conn->close();
